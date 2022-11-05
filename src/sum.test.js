064faa99fb55087fdb5a409e40b46cf523303711
Sergey Kazhaev <surakij6@gmail.com>
2022-11-05 19:30:19 +0400

    set up prettier

--- src/sum.test.js
+++ src/sum.test.js
@@ -1,7 +1,7 @@
-import { sum } from './sum';
+import { sum } from "./sum";
 
-describe('sum', () => {
-  it('should return sum of numbers a & b', () => {
+describe("sum", () => {
+  it("should return sum of numbers a & b", () => {
     expect(sum(1, 2)).toBe(3);
   });
 });
